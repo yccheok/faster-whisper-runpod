32c54f5ce93671ebf33528cb7f8a6ba51f41839e
Yan Cheng Cheok <yancheng.cheok@gmail.com>
2025-02-21 16:35:54 +0800

    Faster whisper integration.

--- src/handler.py
+++ src/handler.py
@@ -1,13 +1,42 @@
 import runpod
+from runpod.serverless.utils import download_files_from_urls, rp_cleanup
+import time
+from faster_whisper import WhisperModel
+
+# Load Faster-Whisper model
+model = WhisperModel("large-v3", device="cuda", compute_type="float16")
 
 def run_faster_whisper_job(job):
-    # For easy migration, we are following the output format of runpod's 
-    # official faster whisper.
-    # https://github.com/runpod-workers/worker-faster_whisper/blob/main/src/predict.py#L111
+    start_time = time.time()
+    
+    job_input = job['input']
+    url = job_input.get('url', "")
+
+    print(f"🚧 Downloading audio from {url}...")
+    audio_path = download_files_from_urls(job['id'], [job_input['audio']])[0]
+    print("✅ Audio downloaded")
+    
+    print("Transcribing...")
+    segments, info = model.transcribe(audio_path, beam_size=5)
+    
+    output_segments = []
+    for segment in segments:
+        output_segments.append({
+            "start": segment.start,
+            "end": segment.end,
+            "text": segment.text
+        })
+    
+    end_time = time.time()
+    time_s = (end_time - start_time)
+    print(f"🎉 Transcription done: {time_s:.2f} s")
+    
     output = {
-        'detected_language' : "",
-        'segments' : ""
+        'detected_language': info.language,
+        'segments': output_segments
     }
+    
+    rp_cleanup(['input_objects'])
 
     return output
 
